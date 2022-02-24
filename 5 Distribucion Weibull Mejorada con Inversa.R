fc398c2ae7f8d2b7e1baa50f4c19e4d6a99494a7
Jonathan Calvopiña <97899559+JonathanCalvo2022@users.noreply.github.com>
2022-02-24 07:10:40 -0500

    Update 5 Distribucion Weibull Mejorada con Inversa.R

--- 5 Distribucion Weibull Mejorada con Inversa.R
+++ 5 Distribucion Weibull Mejorada con Inversa.R
@@ -4,13 +4,13 @@ weibull <- function(n,a,b){
   U <- runif(n)
   X <- (-(1/b^a)*log(U))^(1/a)
 }
-#Ahora probemos para ??=2, ??=3 generemos 1000 valores.
+# Ahora probemos para ??=2, ??=3 generemos 1000 valores.
 X <-weibull(n = 1000,a = 2,b = 3)
-#Realizando un histograma y comparando los resultados tenemos lo siguiente
+# Realizando un histograma y comparando los resultados tenemos lo siguiente
 hist(X,freq = F)
 curve(dweibull(x,shape = 2,scale = 1/3),add=T,col=2)
-#Realizando la prueba de bondad de ajuste se tiene que Kolmogorov-Smirnov
+# Realizando la prueba de bondad de ajuste se tiene que Kolmogorov-Smirnov
 ks.test(X,"pweibull",shape=2,scale=1/3)
 ################################################################################
-p.value> 0.5 No se rechaza la H0(Sigue una distribucion uniforme)
+# p.value> 0.5 No se rechaza la H0(Sigue una distribucion uniforme)
 ################################################################################
