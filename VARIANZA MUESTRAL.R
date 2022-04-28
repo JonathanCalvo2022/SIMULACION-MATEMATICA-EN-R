971120db223feb935566d45c816fe6f84b52a5c2
Jonathan Calvopiña <97899559+JonathanCalvo2022@users.noreply.github.com>
2022-04-28 03:56:58 -0500

    Update VARIANZA MUESTRAL.R

--- VARIANZA MUESTRAL.R
+++ VARIANZA MUESTRAL.R
@@ -1,8 +1,10 @@
+# Funcion Varianza Muestral
 varianza.m <- function(x){
   n <- length(x)
   x.med <- mean(x)
   a<- ((x-x.med)^2)/(n-1)
   sum(a)
 }
+# Datos Muestrales 
 xj <- c(110,97,100,105,108,99,118,104,105,103)
 varianza.m(x = xj)
